ad6bd2f9bd5db3f174cb86f47534571b73dd05db
Megan Jones <38929577+meginks@users.noreply.github.com>
2019-04-05 10:49:24 -0500

    added knex migrations and table creation, made data doc

---
+++ data/migrations/20190405104010_projects_table.js
@@ -0,0 +1,20 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema 
+  .createTable('projects', table => {
+      table.increments(); 
+
+      table.string('project_name', 128) 
+      .notNullable(); 
+
+      table.string('project_description')
+      .notNullable(); 
+
+      table.boolean('completed'); 
+  })
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema 
+  .dropTableIfExists('projects')
+};
---
+++ data/migrations/20190405104027_actions_table.js
@@ -0,0 +1,28 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema
+  .createTable('actions', table => {
+        table.increments(); 
+  
+        table.string('action_description') 
+        .notNullable(); 
+  
+        table.string('action_notes')
+        .notNullable(); 
+  
+        table.boolean('completed'); 
+
+        table.integer('project_id')
+        .unsigned()
+        .notNullable()
+        .references('id')
+        .inTable('projects')
+        .onDelete('CASCADE')
+        .onUpdate('CASCADE'); 
+    })
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema 
+  .dropTableIfExists('actions');
+};
